b01b9769af57f0f9d1020df532c543839fb7b4e6
Ahmed Nafea <ahmednafea.dev@gmail.com>
2024-02-24 22:57:00 +0200

    update structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:go_router/go_router.dart';
-import 'package:repo_rover/application/config/app_constants.dart';
-import 'package:repo_rover/application/config/app_routes.dart';
 import 'package:repo_rover/injection.dart';
+import 'package:repo_rover/presentation/app/app.dart';
 
 Future<void> main() async {
   await setUp();
   runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late final GoRouter _router;
-
-  @override
-  void initState() {
-    super.initState();
-
-    /// Creates a [GoRouter] instance.
-    ///
-    /// Sets initial location set to '/splash',
-    /// debug logging of diagnostics enabled, and navigator key set to
-    /// [_rootNavigatorStateKey]. The routes are set using [AppRoutes.setupAppRoutes],
-    /// with [rootNavigatorStateKey] set to [_rootNavigatorStateKey],
-    _router = GoRouter(
-      initialLocation: '/splash',
-      debugLogDiagnostics: true,
-      navigatorKey: AppConstants.rootNavigatorKey,
-      routes: AppRoutes.setupAppRoutes(
-        rootNavigatorKey: AppConstants.rootNavigatorKey,
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(375, 812),
-      builder: (_, Widget? child) => child!,
-      child: MaterialApp.router(
-        title: 'RepoRover',
-        theme: ThemeData.light(
-          useMaterial3: true,
-        ),
-        debugShowCheckedModeBanner: false,
-        routeInformationParser: _router.routeInformationParser,
-        routerDelegate: _router.routerDelegate,
-        routeInformationProvider: _router.routeInformationProvider,
-      ),
-    );
-  }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:repo_rover/main.dart';
+import 'package:repo_rover/presentation/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
